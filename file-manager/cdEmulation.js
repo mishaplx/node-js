a9d897b2d166de66cf5bf6646d46522d728257d2
Михаил Плехневич <plehnevichma@asup.local>
2022-06-07 17:28:05 +0300

    fix: add cd

--- file-manager/cdEmulation.js
+++ file-manager/cdEmulation.js
@@ -1,5 +1,8 @@
 import currentPath from './currentPath.js';
-export default function cdEmulation(oldPath,newPath){
-  console.log("\x1b[35m",path);
-  currentPath(oldPath + newPath)
+export default function cdEmulation(input){
+  const newp = input.split(' ')
+  console.log(typeof newp);
+   const path = process.env.USERPROFILE + newp[1]
+   console.log("\x1b[35m",path);
+   currentPath(path)
 }
